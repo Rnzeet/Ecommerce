9c8a25b4a0c6f40c67efc462ead92ee5cd6d5176
Rnzeet <ranjitmahato548@gmail.com>
2026-03-01 21:31:12 +0530

    css

--- ecommerce/src/components/ProductCard.jsx
+++ ecommerce/src/components/ProductCard.jsx
@@ -1,66 +1,28 @@
-// // import "./Products.css"
-
-// // function ProductCard({ image, title, description, price }) {
-// //   return (
-// //     <div className="product-card">
-// //       <img src={image} alt={title} />
-
-// //       <div className="card-content">
-// //         <h3 style={{color:"red"}}>{title}</h3>
-// //         <p>{description}</p>
-// //         <div className="price-section">
-// //           <span className="price">${price}</span>
-// //           <button>Add to Cart</button>
-// //         </div>
-// //       </div>
-// //     </div>
-// //   );
-// // }
-
-// // export default ProductCard;
-
-
-// import { useCart } from "../context/CartContext";
-
-// function ProductCard({ product }) {
-//   const { addToCart } = useCart();
-
-//   return (
-//     <div>
-//       <h3>{product.name}</h3>
-//       <p>₹{product.price}</p>
-
-//       <button onClick={() => addToCart(product)}>
-//         Add to Cart
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default ProductCard;
 
 import { useCart } from "../context/CartContext";
 import { toast } from "react-toastify";
-
+import "./ProductsCard.css"
 function ProductCard({ product }) {
   const { addToCart } = useCart();
 
   return (
-    <div className="card">
+    <div className="product-card">
       <img src={product.image} alt={product.title} />
-      <h3>{product.title}</h3>
-      <p>{product.description}</p>
-      <h4>₹{product.price}</h4>
-      <button
-        onClick={() => {
-          addToCart(product);
-          toast.success("Item added to cart 🛒");
-        }}
-      >
-        Add to Cart
-      </button>
-
-
+      <div className="card-content">
+        <h3>{product.title}</h3>
+        <p>{product.description}</p>
+        <div className="price-section">
+          <span className="price">₹{product.price}</span>
+          <button
+            onClick={() => {
+              addToCart(product);
+              toast.success("Item added to cart 🛒");
+            }}
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
