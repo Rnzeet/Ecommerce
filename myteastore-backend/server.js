afe3e0dcd934e14a9656cb4e80ffbe26e7a951f5
Rnzeet <ranjitmahato548@gmail.com>
2026-03-05 15:48:11 +0530

    Added backend API

---
+++ myteastore-backend/server.js
@@ -0,0 +1,17 @@
+console.log("Starting My Tea Store Backend...");
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
