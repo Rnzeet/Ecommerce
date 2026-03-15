b27d48eb29ea98d041812b45c119786d4d980939
Rnzeet <ranjitmahato548@gmail.com>
2026-03-15 12:48:21 +0530

    Refactor ProductList to fetch products from API and remove hardcoded data

--- ecommerce/src/components/ProductList.jsx
+++ ecommerce/src/components/ProductList.jsx
@@ -1,72 +1,27 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./ProductList.css";
 import ProductCard from "./ProductCard";
 
-import img1 from "../assets/image3.jpg";
-import img2 from "../assets/image2.jpg";
-import img3 from "../assets/download.jpg";
-import img4 from "../assets/image3.jpg";
-
 function ProductList() {
-  const products = [
-       {
-  id: 1,
-  image: img1,
-  title: "Cheesy Cheese",
-  description: "Best in the world",
-  price: 99,
-},
-    {
-  id: 1,
-  image: img1,
-  title: "Cheesy Cheese",
-  description: "Best in the world",
-  price: 99,
-},
-     {
-  id: 1,
-  image: img1,
-  title: "Cheesy Cheese",
-  description: "Best in the world",
-  price: 99,
-},
-     {
-  id: 1,
-  image: img1,
-  title: "Cheesy Cheese",
-  description: "Best in the world",
-  price: 99,
-},
-     {
-  id: 1,
-  image: img1,
-  title: "Cheesy Cheese",
-  description: "Best in the world",
-  price: 99,
-},
-     {
-  id: 1,
-  image: img1,
-  title: "Cheesy Cheese",
-  description: "Best in the world",
-  price: 99,
-},
-     {
-  id: 1,
-  image: img1,
-  title: "Cheesy Cheese",
-  description: "Best in the world",
-  price: 99,
-},
+  const [products, setProducts] = useState([]);
 
-  ];
+  const fetchProducts = async () => {
+    const res = await axios.get("http://localhost:5000/admin/products");
+    // Mapping "name" from API to "title" for ProductCard compatibility
+    const fetchedProducts = res.data.map(p => ({ ...p, title: p.name }));
+    setProducts(fetchedProducts);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <div className="home">
       <div className="products-grid">
-        {products.map((item, index) => (
-        <ProductCard product={item} />
-
+        {products.map((item) => (
+          <ProductCard key={item.id} product={item} />
         ))}
       </div>
     </div>
